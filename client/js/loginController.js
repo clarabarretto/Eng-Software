e1bd145b9a490dcd5a5f3407aacaecbf9dd2ac86
clara <clarabarrettoestudo@gmail.com>
2024-03-12 16:02:45 -0300

    ft-starting new design

---
+++ client/js/loginController.js
@@ -0,0 +1,41 @@
+myApp.controller('loginController', ['$scope', '$state', function ($scope, $state) {
+    $scope.user = {
+        email: '',
+        password: ''
+    }
+
+    console.log('conectou');
+    const login = () => {
+        console.log('entrei');
+        // LoginService.getToken($scope.user)
+        //     .then(resp => {
+        //         localStorage.setItem("token", resp.data.token);
+        //         localStorage.setItem("email", $scope.user.email);
+
+        //         if (resp.data.admin) {
+        //             localStorage.setItem("is_admin", true);
+        //         }
+
+        //         $scope.user.email = '';
+        //         $scope.user.password = '';
+
+
+        //         $state.go('all-movies') //só depois
+        //     })
+        //     .catch((err) => {
+        //         const confirmation = Swal.fire({
+        //             title: 'Algo deu errado',
+        //             text: err.data && err.data.error === 'Usuario bloqueado' ? err.data.error : "Verifique as suas informações!",
+        //             icon: 'error',
+        //             confirmButtonColor: '#04052e',
+        //         });
+        //         if (!confirmation.isConfirmed) {
+        //             return;
+        //         }
+        //         localStorage.clear()
+        //         $scope.user.password = ''
+
+        //     })
+    }
+    $scope.login = login
+}])
