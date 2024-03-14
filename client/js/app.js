cefb9ce13f2898f0325c35d8da45210eba0769d2
clara <clarabarrettoestudo@gmail.com>
2024-03-13 23:17:35 -0300

    ft-adding-bootstrap-tpls

--- client/js/app.js
+++ client/js/app.js
@@ -1,14 +1,15 @@
 const myApp = angular.module("doxa", ['ui.router', 'ui.bootstrap']);
 const baseUrl = 'http://localhost:5001/'
 
-myApp.config(function ($stateProvider, $httpProvider) {
-    $httpProvider.interceptors.push('BearerAuthInterceptor');
+myApp.config(function ($stateProvider) {
 
     $stateProvider
         .state({
-            name: 'login',
+            name: 'login-v2',
             url: '',
             templateUrl: 'view/login-v2.html',
             controller: 'loginController'
         })
 })
+
+console.log('myApp');
