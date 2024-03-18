20e7ff7a113e9d0607120318742665f7c57e7872
clara <clarabarrettoestudo@gmail.com>
2024-03-18 15:20:50 -0300

    creating database

--- api/src/config/database.js
+++ api/src/config/database.js
@@ -2,11 +2,7 @@ require('dotenv').config();
 
 module.exports = {
   dialect: 'postgres',
-  host: process.env.DATABASE_HOST,
-  username: process.env.DATABASE_USERNAME,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE,
-  port: process.env.DATABASE_PORT,
+  url: process.env.URL,
   define: {
     timestamps: true,
     underscored: true,
